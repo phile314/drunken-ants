c7318d2a0b5815b52bb9e536ea3a1a6ceac7313f
Philipp Hausmann <philipp.hausmann@314.ch>
2013-09-27 17:15:27 +0200

    -Wrote ast

---
+++ src/antish/Ast.hs
@@ -0,0 +1,40 @@
+
+type Identifier = String
+
+
+data Program = Program StmBlock
+
+data StmBlock = [Statement]
+
+data Statement =
+    FunCall FunIdentifier [Expr]
+  | IfThenElse Expr StmBlock StmBlock
+  | For (Maybe Identifier) Expr StmBlock
+  | Try StmBlock StmBlock StmBlock
+  | Let [Binding] StmBlock
+  | WithProb Double StmBlock StmBlock
+
+data Binding =
+    VarDecl Identifier Expr
+  | FunDecl Identifier [Identifier] StmBlock
+
+data FunIdentifier =
+    VIdent Identifier
+  | CIdent Identifier
+
+data BoolExpr =
+    And Expr Expr
+  | Or  Expr Expr
+  | Not Expr
+
+data Expr =
+    BoolExpr
+  | IntExpr
+--  | ListExpr [Expr]
+--  | FunCall FunIdentifier [Expr]
+  | Prec Expr
+
+
+data IntExpr =
+    ConstInt Integer
+  | VarAccess Identifier
