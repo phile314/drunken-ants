7a0912a9d3019ce161d7ba10347ec351c8e1eb93
Marco Vassena <m.vassena@students.uu.nl>
2013-09-30 14:50:19 +0200

    Modified Ast, start writing parser with Parsec

--- src/antish/Ast.hs
+++ src/antish/Ast.hs
@@ -5,38 +5,39 @@ module Ast where
 type Identifier = String
 
 data Program = Program StmBlock
+  deriving (Show, Eq)
 
 data StmBlock = StmBlock [Statement]
+  deriving (Show, Eq)
 
 data Statement =
     FunCall FunIdentifier [Expr]
-  | IfThenElse Expr StmBlock StmBlock
+  | IfThenElse BoolExpr StmBlock StmBlock
   | For (Maybe Identifier) Expr StmBlock
   | Try StmBlock StmBlock StmBlock
   | Let [Binding] StmBlock
   | WithProb Double StmBlock StmBlock
+  deriving (Show, Eq)
 
 data Binding =
     VarDecl Identifier Expr
   | FunDecl Identifier [Identifier] StmBlock
+  deriving (Show, Eq)
 
 data FunIdentifier =
     VIdent Identifier
   | CIdent Identifier
+  deriving (Show, Eq)
 
 data BoolExpr =
-    And Expr Expr
-  | Or  Expr Expr
-  | Not Expr
+    And BoolExpr BoolExpr
+  | Or  BoolExpr BoolExpr
+  | Not BoolExpr
+  deriving (Show, Eq)
 
 data Expr =
-    BoolExpr
-  | IntExpr
+  ConstInt Integer
 --  | ListExpr [Expr]
 --  | FunCall FunIdentifier [Expr]
-  | Prec Expr
-
-
-data IntExpr =
-    ConstInt Integer
   | VarAccess Identifier
+  deriving (Show, Eq)
