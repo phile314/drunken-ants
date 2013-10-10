cfa4879f724b68de2833d6e2a85e48bda83dbb22
Marco Vassena <m.vassena@students.uu.nl>
2013-10-10 11:12:16 +0200

    [Green] Completed exhaustive test for basic conditions

--- src/antish/testsuite/parser/BoolTest.hs
+++ src/antish/testsuite/parser/BoolTest.hs
@@ -1,9 +1,16 @@
 -- Tests for boolean exppressions parser
 
+{-# LANGUAGE BangPatterns #-}
+
 module Main where
 
 import Test.HUnit
 import System.Exit (exitFailure)
+import Assembly (Cond (..), SenseDir (..))
+import Parser.Boolean
+import Ast
+import Text.Parsec
+import Data.Either 
 
 -- | The entry point of the test suite
 main :: IO ()
@@ -14,7 +21,16 @@ main = do
     else return ()
 
 tests :: Test
-tests = TestList [] 
+tests = TestList [simpleCondition] 
 
+-- tests that all the basic conditions are parsed correctly
 simpleCondition :: Test
-simpleCondition = undefined
+simpleCondition = TestList $ [ expected x y ~=? actual x y | (x,y) <- bc]
+   where bc = [(c,s) | c <- condition, s <- sensedir]
+         sensedir = [Here, Ahead, LeftAhead, RightAhead]
+         condition = [Friend, Foe, FriendWithFood, FoeWithFood, Food, Rock, 
+                      FoeMarker, Home, FoeHome] ++ [Marker i | i <- [0..5]]
+         expected x y = Condition x y
+         actual x y = let input = show x ++ " " ++ show y
+                          result = parse pBoolExpr input input in
+                            either (error . show) id result
